2cc870abfcb021454ed304d59b6ae5cb70f88146
DannyTrany <124431889+DannyTrany@users.noreply.github.com>
2023-02-26 09:55:17 +0600

    Update Animal.java

--- problem-B/Animal.java
+++ problem-B/Animal.java
@@ -11,3 +11,83 @@ public abstract class Animal {
     }
     public abstract void eat();
 }
+
+class Spider extends Animal{
+
+    public Spider(){
+        super(8);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Spider eats insects.");
+    }
+}
+
+interface Pet {
+    public String getName();
+    public void setName(String name);
+    void play();
+}
+
+class Cat extends Animal implements Pet {
+    private String pet_name;
+
+    public Cat(String name) {
+        super(4);
+        pet_name = name;
+    }
+
+    public Cat() {
+        this("");
+    }
+
+    public void eat() {
+        System.out.println("The cat eats food");
+    }
+
+    @Override
+    public String getName() {
+        return pet_name;
+    }
+
+    @Override
+    public void setName(String name) {
+        pet_name = name;
+    }
+
+    public void play() {
+        System.out.println(pet_name + " plays with a ball");
+    }
+}
+
+class Fish extends Animal{
+
+    public Fish() {
+        super(0);
+    }
+    @Override
+    public void eat() {
+        System.out.println("The fish eats fish food.");
+    }
+    @Override public void walk() {
+        System.out.println("Fish can't walk!");
+    }
+}
+
+class TestAnimals {
+    public static void main(String[] args) {
+        Fish fish = new Fish();
+        Cat cat = new Cat("Mysyq");
+        Animal animal = new Fish();
+        Animal animal2 = new Spider();
+
+        Pet p = new Cat();
+        p.play();
+
+        fish.eat();
+        cat.eat();
+        animal.walk();
+        animal2.walk();
+    }
+}
