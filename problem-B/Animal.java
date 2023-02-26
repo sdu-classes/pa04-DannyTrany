0a87f8a74d698ccd32bc5bb7b370da2e810a4f93
DannyTrany <124431889+DannyTrany@users.noreply.github.com>
2023-02-26 09:50:30 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,13 @@
+package ProblemB;
+
+public abstract class Animal {
+    protected int legs;
+
+    protected Animal(int legs){
+        this.legs = legs;
+    }
+    public void walk(){
+        System.out.println("This animal walks on " + legs + " legs");
+    }
+    public abstract void eat();
+}
